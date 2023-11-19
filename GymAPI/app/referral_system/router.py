e9e06385e265c89a350e859ecb4e01b48e2b1c65
DanPoroshin <oksanochaike@gmail.com>
2023-11-19 13:42:17 +0500

    Update

--- GymAPI/app/referral_system/router.py
+++ GymAPI/app/referral_system/router.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.database import get_async_session
-from app.auth.models import User
+from app.auth.models import User, user
 from app.referral_system.utils import get_user_by_referral_code
 from app.referral_system.models import Referral
 from app.auth.base_config import current_user
@@ -14,13 +14,7 @@ router = APIRouter(
 )
 
 
-@router.get("/get_user_by_referral")
-async def get_user_by_referral(referral_code: str, session: AsyncSession = Depends(get_async_session)):
-    user_id = await get_user_by_referral_code(referral_code, session)
-    return user_id
-
-
-@router.post("/referral", response_model=None)
+@router.post("/submit_referral", response_model=None)
 async def add_referral_operation(referral_code: str, session: AsyncSession = Depends(get_async_session), curr_user: User = Depends(current_user)):
     referral_user_id = await get_user_by_referral_code(referral_code, session)
     curr_user_id = curr_user.id
