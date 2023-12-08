e73ce10ab8f0a171ec22b084889852b577c1ed08
DanPoroshin <oksanochaike@gmail.com>
2023-12-08 23:15:28 +0500

    Some fixes

--- GymAPI/app/referral_system/utils.py
+++ GymAPI/app/referral_system/utils.py
@@ -1,4 +1,4 @@
-from sqlalchemy import select
+from sqlalchemy import select, and_
 from sqlalchemy.sql import func
 
 from app.auth.models import user
@@ -13,15 +13,17 @@ async def get_user_by_referral_code(referral_code: str, session) -> int:
 
 
 async def referral_info(session, curr_user) -> dict:
-    query = select(func.count()).where(
-        referral.c.referral_owner_id == curr_user.id and referral.c.used == True)
+    query = select(referral).where(
+        and_(referral.c.referral_owner_id ==
+             curr_user.id, referral.c.used == True)
+    )
     result = await session.execute(query)
     active_referrals = result.scalar()
 
     response = {
         "referral_code": curr_user.referral_code,
         "referral_code_used_count": curr_user.referral_code_used_count,
-        "active_referrals": active_referrals,
+        "active_referrals": active_referrals if active_referrals else 0,
     }
 
     return response
